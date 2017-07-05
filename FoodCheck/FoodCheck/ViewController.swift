1b575da97750a9919b66c3e57ddca7e74006ada3
passt0r <dima3@ymail.com>
2017-07-05 21:31:50 +0300

    Initial commit

---
+++ FoodCheck/FoodCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodCheck
+//
+//  Created by Dmytro Pasinchuk on 05.07.17.
+//  Copyright © 2017 Dmytro Pasinchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
